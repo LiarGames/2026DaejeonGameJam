f4bab72c6f393c4dd2ebb391f077d4ceabae5719
Paul Bin <paul.bin0117@gmail.com>
2026-08-08 13:18:19 +0900

    modified player collider

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -76,9 +76,9 @@ public class PlayerStats : MonoBehaviour
 
         Debug.Log($"Player leveled up to level {Level}");
 
-        UpgradeStat(StatType.Attack);
-        UpgradeStat(StatType.MaxHealth);
-        UpgradeStat(StatType.MaxMana);
+        // UpgradeStat(StatType.Attack);
+        // UpgradeStat(StatType.MaxHealth);
+        // UpgradeStat(StatType.MaxMana);
     }
 
     public void TakeDamage(float amount)
